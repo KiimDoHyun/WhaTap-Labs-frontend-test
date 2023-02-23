c383f6c6ccc0a6e99e84a735064c82a58b0bad37
KDH20160140 <bzidol@naver.com>
2023-02-23 19:53:39 +0900

    feat: 차트 모양 변경, 값 텍스트 추가

--- src/component/TestChart1.tsx
+++ src/component/TestChart1.tsx
@@ -46,18 +46,22 @@ const TestChart1 = () => {
         };
     };
 
+    /*
+    값 중복이 있는 경우 어떻게 고유하게 값을 지정할지
+
+    12
+    7
+    7
+    4
+
+    -> 7 값이 서로다른 2개가 아닌 한개의 막대차트만 그림
+    */
     const updateChart = (newData: any) => {
         const svg: any = select(svgRef.current);
 
         const xScale = scaleBand() // x 축
-            .domain(newData.map((item: any) => String(item.value)))
+            .domain(newData.map((item: any) => String(item.name)))
             .range([50, 450]);
-        // .padding(0.5);
-
-        const xAxis = axisBottom(xScale).ticks(4);
-        svg.select(".x-axis")
-            .style("transform", "translateY(450px)")
-            .call(xAxis);
 
         svg.selectAll(".bar")
             .data(newData)
@@ -65,26 +69,28 @@ const TestChart1 = () => {
             .transition()
             .duration(500)
             // 모든 데이터가 변경되는 경우 딜레이를 추가할 수 있음.
-            .delay((d: any, i: any) => i * 100)
+            // .delay((d: any, i: any) => i * 100)
             .attr("class", "bar")
             // 첫번째 인자에 배열의 요소가, 두번째 인자에 인덱스가 들어있음.
-            .attr("height", function (d: any, i: any) {
+            .attr("width", function (d: any, i: any) {
                 return d.value;
             }) // 높이는 각 값의 *5 만큼 크기로
-            .attr("width", 25) // 너비는 25로
-            .attr("x", function (d: any, i: any) {
-                console.log(i * 25);
-                console.log("? value22", xScale("0"));
-                // return i * 25;
-                return xScale(String(d.value));
-
-                console.log("? value22", xScale(d.value));
-                console.log("? name22", xScale(d.name));
+            .attr("height", 25) // 너비는 25로
+            .attr("y", function (d: any, i: any) {
                 return xScale(d.name) + 25;
             }) // x 위치는 해당 값의 x축의 위치로
-            .attr("y", function (d: any, i: any) {
-                return 450 - d.value;
+            .attr("x", function (d: any, i: any) {
+                return 50;
             }); // y 는 원래 높이에서 해당 높이를 뺀 만큼
+
+        svg.selectAll(".text")
+            .data(newData)
+            .transition()
+            .duration(500)
+            .attr("class", "text")
+            .text(function (d: any) {
+                return d.value;
+            });
     };
 
     // 필요데이터 조회: act (액티브 스테이터스)
@@ -109,16 +115,20 @@ const TestChart1 = () => {
         const svg: any = select(svgRef.current);
 
         const xScale = scaleBand() // x 축
-            .domain(data.map((item) => String(item.value)))
+            // .domain(
+            //     data.map((item) => `${item.name}-${item.value}`.split("-")[1])
+            // )
+            .domain(data.map((item) => `${item.name}`))
             .range([50, 450]);
 
         const yScale = scaleLinear() // y 축
             .domain([0, 200]) // 실제값의 범위, // 최대값 찾아서 범위 지정?
             .range([450, 50]); // 차트를 그리기 위해 크기를 지정.
 
-        const xAxis = axisBottom(xScale).ticks(4);
+        const xAxis = axisLeft(xScale).ticks(4);
         svg.select(".x-axis")
-            .style("transform", "translateY(450px)")
+            // .style("transform", "translateY(450px)")
+            .attr("transform", "translate(50, 0)")
             .call(xAxis);
 
         // const yAxis = axisLeft(yScale);
@@ -127,34 +137,73 @@ const TestChart1 = () => {
         //     .attr("transform", "translate(50, 0)")
         //     .call(yAxis);
 
-        svg.selectAll(".bar")
+        // 텍스트 추가해보기
+
+        let bar = svg
+            .selectAll(".item")
             .data(data)
             .enter()
-            .append("rect")
-            // 작동 X
-            // .transition()
-            // .duration(500)
+            .append("g")
+            .attr("class", "item");
+
+        bar.append("rect")
             .attr("class", "bar")
             // 첫번째 인자에 배열의 요소가, 두번째 인자에 인덱스가 들어있음.
-            .attr("height", function (d: any, i: any) {
+            .attr("width", function (d: any, i: any) {
                 return d.value;
             }) // 높이는 각 값의 *5 만큼 크기로
-            .attr("width", 25) // 너비는 25로
-            .attr("x", function (d: any, i: any) {
-                return i * 25;
-                console.log("? value", xScale(d.value)!);
-                console.log("? name", xScale(d.name)!);
-                return xScale(d.value);
-                // return xScale(d.name) + 25;
-            }) // x 위치는 해당 값의 x축의 위치로
+            .attr("height", 25) // 너비는 25로
             .attr("y", function (d: any, i: any) {
-                return 450 - d.value;
+                // return i * 25;
+                // console.log("? value", xScale(d.value)!);
+                // console.log("? name", xScale(d.name)!);
+                // return xScale(d.value);
+                return xScale(d.name) + 25;
+            }) // x 위치는 해당 값의 x축의 위치로
+            .attr("x", function (d: any, i: any) {
+                return 50;
             }); // y 는 원래 높이에서 해당 높이를 뺀 만큼
-    }, []);
 
-    useEffect(() => {
-        console.log("data: ", data);
-    }, [data]);
+        bar.append("text")
+            .attr("class", "text")
+            .text(function (d: any) {
+                return d.value;
+            })
+            .attr("fill", "#919191")
+            .attr("x", 60)
+            .attr("y", function (d: any, i: any) {
+                // return i * 25;
+                // console.log("? value", xScale(d.value)!);
+                // console.log("? name", xScale(d.name)!);
+                // return xScale(d.value);
+                return xScale(d.name) + 42;
+            });
+
+        // 기존
+        // svg.selectAll(".bar")
+        //     .data(data)
+        //     .enter()
+        //     .append("rect")
+        //     // 작동 X
+        //     // .transition()
+        //     // .duration(500)
+        //     .attr("class", "bar")
+        //     // 첫번째 인자에 배열의 요소가, 두번째 인자에 인덱스가 들어있음.
+        //     .attr("width", function (d: any, i: any) {
+        //         return d.value;
+        //     }) // 높이는 각 값의 *5 만큼 크기로
+        //     .attr("height", 25) // 너비는 25로
+        //     .attr("y", function (d: any, i: any) {
+        //         // return i * 25;
+        //         // console.log("? value", xScale(d.value)!);
+        //         // console.log("? name", xScale(d.name)!);
+        //         // return xScale(d.value);
+        //         return xScale(d.name) + 25;
+        //     }) // x 위치는 해당 값의 x축의 위치로
+        //     .attr("x", function (d: any, i: any) {
+        //         return 50;
+        //     }); // y 는 원래 높이에서 해당 높이를 뺀 만큼
+    }, []);
 
     /*
     크기
@@ -187,6 +236,12 @@ const TestChart1 = () => {
                 <svg ref={svgRef} style={{ height: "100%", width: "100%" }}>
                     <g className="y-axis" />
                     <g className="x-axis" />
+                    {/* <g>
+                        <rect x={60} y={50} width={100} height={25}></rect>
+                        <text x={60} y={50} fill={"red"}>
+                            hihihihihi
+                        </text>
+                    </g> */}
                 </svg>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -6,21 +6,23 @@ import reportWebVitals from "./reportWebVitals";
 
 export const arr = [];
 
+let isWorking = false;
+
 // 체크: 0.1초 단위
 // 0.01초 단위로 하면 에러 발생
 // 이전 호출이 종료되고 호출 되어야 함.
 const apiManager = () => {
     setInterval(async () => {
         if (arr.length > 0) {
-            console.log("처리할 데이터가 존재합니다.", arr);
-            console.log("대상: ", arr[0]);
             try {
                 const result = await arr[0].callApi();
                 arr[0].success(result.data);
             } catch (e) {
                 console.log(e);
                 console.log("에러 발생", arr);
-                arr[0].fail();
+                if (arr.length > 0) {
+                    arr[0].fail();
+                }
             } finally {
                 arr.shift();
             }
