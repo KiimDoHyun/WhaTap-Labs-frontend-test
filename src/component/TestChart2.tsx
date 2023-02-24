9f9440def86cd3091a9a537c8da47f831a2c097a
KDH20160140 <bzidol@naver.com>
2023-02-24 22:47:26 +0900

    feat: 차트 x축 실시간 변경 테스트

--- src/component/TestChart2.tsx
+++ src/component/TestChart2.tsx
@@ -1,12 +1,18 @@
 import {
+    active,
     axisBottom,
     axisLeft,
     curveMonotoneX,
+    easeLinear,
     line,
+    max,
+    randomNormal,
     randomUniform,
     range,
     scaleLinear,
+    scaleTime,
     select,
+    timeParse,
 } from "d3";
 import React, { useEffect, useRef, useState } from "react";
 import api from "../api";
@@ -15,32 +21,30 @@ const TestChart2 = () => {
     const svgRef = useRef(null);
     const [data, setData] = useState(null);
 
-    /*
-    1. 라인차트 최대값 기준?
-    */
-
-    // 라인차트 데이터
-    useEffect(() => {
-        api.series("transaction/{stime}/{etime}", {
-            // stime: Date.now() - HOUR,
-            // stime: Date.now() - 1000 * 5,
-            stime: Date.now() - 1000 * 5,
-            // stime: Date.now() - 1000 * 60 * 5,
-            etime: Date.now(),
-        }).then((result) => {
-            // console.log(new Date(Date.now() - 5000));
-            console.log("result", result.data);
-            // setHttpcSeries(result);
-        });
-
+    const draw1 = () => {
         const svg: any = select(svgRef.current);
 
-        let n = 10;
+        let n = 100;
 
         // Max값의 기준?
-        let xScale = scaleLinear().domain([0, 10]).range([50, 450]);
+        // x 축 시간 범위로 변경
+        // 조회 데이터엔 시간값이 없음
+        // 범위 시간만 알 수 있음.
+        // 시작, 종료 시간에서 5초 단위로 끊어서 데이터를 조합?
 
-        // y 축은 시간을 나타내야 한다.
+        // let xScale = scaleLinear()
+        //     // .domain([
+        //     //     new Date(Date.now() - 1000 * 60 * 10),
+        //     //     new Date(Date.now()),
+        //     // ])
+        //     .domain(aa)
+        //     .range([50, 450]);
+
+        //
+
+        // 시간축
+        let xScale = scaleLinear().domain([0, n]).range([50, 450]);
+
         let yScale = scaleLinear().domain([0, 1]).range([450, 50]);
 
         let myLine = line()
@@ -49,12 +53,11 @@ const TestChart2 = () => {
             })
             .y(function (d: any, i) {
                 return yScale(d.y);
-            });
-        // .curve(curveMonotoneX);
+            })
+            .curve(curveMonotoneX);
 
-        let dataSet = range(n).map((d) => ({ y: randomUniform(1)() }));
+        let dataSet = range(n).map((d) => ({ y: randomUniform(0.3, 0.5)() }));
 
-        console.log(dataSet);
         svg.select(".x-axis")
             .attr("transform", "translate(0, 450)")
             .call(axisBottom(xScale));
@@ -73,25 +76,142 @@ const TestChart2 = () => {
             .attr("d", myLine);
 
         // 점 추가
-        svg.selectAll(".dot")
-            .data(dataSet)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d: any, i: any) {
-                return xScale(i);
+        // svg.selectAll(".dot")
+        //     .data(dataSet)
+        //     .enter()
+        //     .append("circle")
+        //     .attr("cx", function (d: any, i: any) {
+        //         return xScale(i);
+        //     })
+        //     .attr("cy", function (d: any, i: any) {
+        //         return yScale(d.y);
+        //     })
+        //     .attr("r", 3);
+    };
+
+    const draw2 = () => {
+        const svg: any = select(svgRef.current);
+
+        var n = 243,
+            duration = 750,
+            now: any = new Date(Date.now() - duration),
+            count = 0,
+            data = range(n).map(function () {
+                return 0;
+            });
+
+        var margin = { top: 20, right: 20, bottom: 20, left: 40 },
+            width = +svg.attr("width") - margin.left - margin.right,
+            height = +svg.attr("height") - margin.top - margin.bottom,
+            g = svg
+                .append("g")
+                .attr(
+                    "transform",
+                    "translate(" + margin.left + "," + margin.top + ")"
+                );
+
+        var x: any = scaleTime()
+            .domain([now - (n - 2) * duration, now - duration])
+            .range([0, width]);
+
+        var y = scaleLinear().range([height, 0]);
+
+        var myLine: any = line()
+            .x(function (d, i: any) {
+                return x(now - (n - 1 - i) * duration);
             })
-            .attr("cy", function (d: any, i: any) {
-                return yScale(d.y);
-            })
-            .attr("r", 5);
+            .y(function (d: any, i) {
+                return y(d);
+            });
+
+        g.append("defs")
+            .append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+
+        var axis = g
+            .append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call((x.axis = axisBottom(x)));
+
+        g.append("g") // y
+            .attr("class", "axis axis--y")
+            .call(axisLeft(y));
+
+        g.append("g")
+            .attr("clip-path", "url(#clip)")
+            .append("path")
+            .datum(data)
+            .attr("class", "line") // (CSS)
+            .transition()
+            .duration(750)
+            .ease(easeLinear)
+            .on("start", tick);
+
+        function tick(this: any) {
+            now = new Date();
+            x.domain([now - (n - 2) * duration, now - duration]);
+            y.domain([0, max(data)]);
+            data.push(Math.min(30, count));
+            count = 0;
+
+            select(this).attr("d", myLine).attr("transform", null);
+
+            axis.transition().duration(750).ease(easeLinear).call(x.axis);
+
+            active(this)
+                .attr(
+                    "transform",
+                    "translate(" + x(now - (n - 1) * duration) + ")"
+                )
+                .transition()
+                .on("start", tick);
+
+            data.shift();
+        }
+    };
+
+    /*
+    1. 라인차트 최대값 기준?
+    */
+
+    // 라인차트 데이터
+    // 초기에 10분 단위를 조회해서 보여준다.
+    // 이후 5초 단위로 조회해서 차트를 갱신한다. -> 데이터가 없음. 어떻게?
+    useEffect(() => {
+        const start = Date.now() - 1000 * 5;
+        // const start = Date.now() - 1000 * 60 * 5;
+        const end = Date.now();
+        api.series("transaction/{stime}/{etime}", {
+            stime: start,
+            etime: end,
+        }).then((result) => {
+            // console.log(new Date(start), new Date(end));
+            console.log("result", result.data);
+        });
+
+        // 정적 차트
+        // draw1();
+
+        // 동적 차트
+        draw2();
     }, []);
+
     return (
         <div>
             <div>
                 Time <div className="infoIcon"></div>
             </div>
             <div style={{ height: "500px", width: "500px" }}>
-                <svg ref={svgRef} style={{ height: "100%", width: "100%" }}>
+                <svg
+                    width={500}
+                    height={500}
+                    ref={svgRef}
+                    style={{ height: "500px", width: "500px" }}
+                >
                     <g className="y-axis" />
                     <g className="x-axis" />
                 </svg>
