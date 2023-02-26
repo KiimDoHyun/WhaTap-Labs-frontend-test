55b972505a300b5974bdce12ef3dd50d25152a30
KDH20160140 <bzidol@naver.com>
2023-02-26 14:42:22 +0900

    feat: 인포메틱스 차트 추가

--- src/component/TestChart3.tsx
+++ src/component/TestChart3.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { queue } from "..";
 import api from "../api";
 
@@ -6,18 +7,22 @@ const TestChart3 = () => {
     const [data, setData] = useState([
         {
             name: "act_agent",
+            title: "활성화 상태의 에이전트 수",
             value: 0,
         },
         {
             name: "inact_agent",
+            title: "비활성화 상태의 에이전트 수",
             value: 0,
         },
         {
             name: "host",
+            title: "호스트 수",
             value: 0,
         },
         {
             name: "cpucore",
+            title: "호스트의 CPU 코어 합",
             value: 0,
         },
     ]);
@@ -55,7 +60,38 @@ const TestChart3 = () => {
     useEffect(() => {
         console.log("data :", data);
     }, [data]);
-    return <>TestChart3</>;
+    return (
+        <TestChart3Box>
+            {data.map((item) => (
+                <InformaticsBox key={item.name}>
+                    <div className="title">{item.title}</div>
+                    <div className="value">{item.value}</div>
+                </InformaticsBox>
+            ))}
+        </TestChart3Box>
+    );
 };
+
+const TestChart3Box = styled.div`
+    width: 100px;
+    box-sizing: border-box;
+    border-top: 1px solid;
+    border-left: 1px solid;
+    border-right: 1px solid;
+`;
+
+const InformaticsBox = styled.div`
+    border-bottom: 1px solid;
+    padding: 8px;
+    box-sizing: border-box;
+
+    .title {
+        font-size: 12px;
+    }
+
+    .value {
+        text-align: right;
+    }
+`;
 
 export default TestChart3;
