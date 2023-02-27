25f9aa1fc68489d96c6307480e93d96cbcf766b0
KDH20160140 <bzidol@naver.com>
2023-02-27 12:09:22 +0900

    refactor: 바 차트 바인딩 데이터 일반 변수로 변경

--- src/component/BarChart.tsx
+++ src/component/BarChart.tsx
@@ -1,5 +1,5 @@
 import { axisLeft, axisTop, max, scaleBand, scaleLinear, select } from "d3";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { queue } from "..";
 import api from "../api";
 import styled from "styled-components";
@@ -7,33 +7,33 @@ import useResize from "../hook/useResize";
 
 const margin = { top: 20, right: 20, bottom: 20, left: 70 };
 
+let data = [
+    {
+        name: "act_method",
+        value: 0,
+    },
+    {
+        name: "act_sql",
+        value: 0,
+    },
+    {
+        name: "act_httpc",
+        value: 0,
+    },
+    {
+        name: "act_dbc",
+        value: 0,
+    },
+    {
+        name: "act_socket",
+        value: 0,
+    },
+];
+
 const BarChart = () => {
     const svgRef = useRef(null);
     const svgParentBoxRef = useRef(null);
     const size = useResize(svgParentBoxRef);
-
-    const [data, setData] = useState([
-        {
-            name: "act_method",
-            value: 0,
-        },
-        {
-            name: "act_sql",
-            value: 0,
-        },
-        {
-            name: "act_httpc",
-            value: 0,
-        },
-        {
-            name: "act_dbc",
-            value: 0,
-        },
-        {
-            name: "act_socket",
-            value: 0,
-        },
-    ]);
 
     // resize
     // !데이터 바인딩 함수와 중복되는 부분 다수 존재
@@ -109,14 +109,14 @@ const BarChart = () => {
     };
 
     // 데이터 바인딩
-    const updateChart = (newData: any) => {
+    const updateChart = () => {
         const svg: any = select(svgRef.current);
 
         const parentWidth = svgParentBoxRef.current.offsetWidth;
 
         const width = parentWidth - margin.left - margin.right;
 
-        const yMax = max(newData, (d: any) => d.value);
+        const yMax = max(data, (d: any) => d.value);
 
         const yScale = scaleLinear()
             .domain([0, Number(yMax)])
@@ -127,7 +127,7 @@ const BarChart = () => {
 
         // 막대차트 길이 갱신
         svg.selectAll(".bar")
-            .data(newData)
+            .data(data)
             .transition()
             .duration(500)
             .attr("width", function (d: any) {
@@ -136,7 +136,7 @@ const BarChart = () => {
 
         // 텍스트 갱신
         svg.selectAll(".text")
-            .data(newData)
+            .data(data)
             .transition()
             .duration(500)
             .text((d: any) => d.value);
@@ -147,22 +147,18 @@ const BarChart = () => {
     const apiObj = (type: string) => {
         return {
             callApi: () => api.spot(type),
-            success: (newValue: any) =>
-                setData((prevState) => {
-                    const newData = prevState.map((item) =>
-                        item.name === type ? { ...item, value: newValue } : item
-                    );
-                    updateChart(newData);
-
-                    return newData;
-                }),
+            success: (newValue: any) => {
+                data = data.map((item) =>
+                    item.name === type ? { ...item, value: newValue } : item
+                );
+                updateChart();
+            },
             fail: () => console.warn("api 호출에 실패했습니다."),
         };
     };
 
     // 필요데이터 조회: act (액티브 스테이터스)
     // 최대값 기준
-    const isfirst = useRef(true);
     useEffect(() => {
         setInterval(() => {
             queue.push(apiObj("act_method"));
