f41b23c8ff057a64d2efaf9ed4a66471673e52b4
KDH20160140 <bzidol@naver.com>
2023-03-09 22:38:10 +0900

    feat: apiManager clearInterval 추가

--- src/index.js
+++ src/index.js
@@ -7,66 +7,43 @@ import { BrowserRouter } from "react-router-dom";
 
 const queue = [];
 
-export const enqueueApi = (item) => {
-    queue.push(item);
-
-    // startInterval();
-
-    // queue에 아이템이 추가되면 interval을 시작한다.
-    // queue가 비어있으면 interval을 종료한다.
-    // 중간에 에러가 발생한다면?
-};
-
-/*
-startInterval () {
-    interval.run(queue);
-
-    intervalCallback() {
-        try {
-            callApi()
-        }
-        catch {
-            failApiList.push(failApi)
-        }
-        finally {
-            queue.shift()
-        }
-    }
-
-
-    if(failApiList.length > 0) {
-        interval.run(failApiList)
-    }
-}
-*/
+let interval = null;
 
 let isWorking = false;
 
-// 체크: 0.1초 단위
-// 0.01초 단위로 하면 에러 발생
-// 이전 호출이 종료되고 호출 되어야 함.
-const apiManager = () => {
-    setInterval(async () => {
-        if (queue.length > 0 && isWorking === false) {
-            try {
-                isWorking = true;
-                const result = await queue[0].callApi();
-                queue[0].success(result);
-            } catch (e) {
-                console.log(e);
-                console.log("에러 발생", queue);
-                if (queue.length > 0) {
-                    queue[0].fail();
-                }
-            } finally {
-                isWorking = false;
-                queue.shift();
-            }
-        }
-    }, 10);
+const clearApiInterval = () => {
+    clearInterval(interval);
 };
 
-apiManager();
+export const enqueueApi = (item) => {
+    // 등록
+    if (queue.length === 0) {
+        queue.push(item);
+
+        interval = setInterval(async () => {
+            if (queue.length > 0 && isWorking === false) {
+                try {
+                    isWorking = true;
+                    const result = await queue[0].callApi();
+                    queue[0].success(result);
+                } catch (e) {
+                    console.log(e);
+                    console.log("에러 발생", queue);
+                    if (queue.length > 0) {
+                        queue[0].fail();
+                    }
+                } finally {
+                    isWorking = false;
+                    queue.shift();
+                }
+            } else if (queue.length === 0) {
+                clearApiInterval();
+            }
+        }, 10);
+    } else {
+        queue.push(item);
+    }
+};
 
 ReactDOM.render(
     <React.StrictMode>
